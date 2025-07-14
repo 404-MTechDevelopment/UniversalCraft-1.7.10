6579f8f1d1919fbb96402ad8c3c9d72f3fef1d7d
MTechLab Dev <dliapromua@gmail.com>
2025-07-14 16:15:12 +0300

    1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,10 +24,7 @@ rootProject.buildFileName = "root.gradle.kts"
 listOf(
     "1.7.10",
     "1.8.9",
-    "1.12.2",
-    "1.15.2",
-    "1.16.2",
-    "1.16.2-fabric"
+    "1.12.2"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
